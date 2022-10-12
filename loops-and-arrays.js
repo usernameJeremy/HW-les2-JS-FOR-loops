747819c5ff91fd7fe5ef7934a6474f6f50e15499
usernameJeremy <jeremy_koster@outlook.com>
2022-10-12 11:47:25 +0200

    huiswerk klaar

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -6,6 +6,10 @@
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
 
+for (let i = 0; i < names.length ; i++) {
+    names[i]=names[i]+"je"
+}
+console.log(names)
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
@@ -23,6 +27,14 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 
 const numbers = [2, 4, 5, 29, 38];
 
+for (let i = 0; i < numbers.length ; i++) {
+    if (numbers[i] % 2 === 0){
+      numbers[i]=numbers[i] * 2;
+    }else {
+    numbers[i] =numbers[i] * 3;
+    }
+}
+console.log(numbers);
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
@@ -41,6 +53,14 @@ const numbers = [2, 4, 5, 29, 38];
 // ==========================================
 
 const squares = [30, 2, 8, 24, 11];
+
+for (let i = 0; i <squares.length ; i++) {
+
+
+    squares[i]="het volume van "+squares[i]+" is "+squares[i]*squares[i]*squares[i];
+}
+console.log(squares)
+
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
--- loops.js
+++ loops.js
@@ -6,7 +6,8 @@
 // Hoera!
 // ==========================================
 
-
+for(let i = 0; i < 3; i++){
+console.log( "Hoera!") }
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -19,6 +20,10 @@
  // ==========================================
 
 
+for(let i = 0; i < 4; i++){
+    console.log( "loop...")
+}
+console.log("klaar!");
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -29,10 +34,14 @@
 // 3 is oneven
 // .... etc.
 // 15 is oneven
+for (let i = 0; i < 16; i++) {
+    if (i % 2 === 0){
+    console.log(i + " is even ")
+    }else (console.log(i + " is oneven"))
 
+}
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
-
 
 
 // ==========================================
@@ -50,4 +59,12 @@
 // >> 8
 // >> 9
 // ==========================================
-
+for (let i = 0; i <= 9; i++) {
+    if(i < 3) {
+        console.log(i)
+    } else if( i > 2 && i<6) {
+        console.log("> "+i)
+    } else{
+        console.log(">> "+i)
+    }
+}
